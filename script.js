0cae6a16611de8f0789db87073f8c08c97ff85eb
Infectos <infectos@yandex.ru>
2019-12-11 22:35:44 +0300

    comments added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// createCell creates one 'cell' div inside of the "grid" div
 function createCell(number) {
 
     let newCell = document.createElement('div');
@@ -8,6 +9,8 @@ function createCell(number) {
     const grid = document.querySelector('#fieldGrid');
     grid.appendChild(newCell);
 }
+
+// resize sets properties of 'grid' div and fill it with new cells created by func "createCell"
 
 function resize(size = 16) {
 
@@ -35,15 +38,17 @@ function resize(size = 16) {
 
 }
 
+// resize button now asks the size of the field and do resize
 const resetBtn = document.querySelector('#fieldBtn');
 resetBtn.addEventListener('click', () => {
         resize(Number(prompt('Pick a field size. It must be from 1 to 99 cells.')));
     }
-
 );
 
+// This creates the first field
 resize();
 
+// clearBtn sets colors of all cells to default value
 const clearBtn = document.querySelector('#clearBtn');
 clearBtn.addEventListener('click', () => {
     const cells = document.querySelectorAll('.cell');
@@ -53,15 +58,15 @@ clearBtn.addEventListener('click', () => {
     })
 })
 
-
+// This applies 'randomen' func listener to all cells
 function randomColor() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
-
         cell.addEventListener('mouseenter', randomen)
     })
 }
 
+// This creates a random color and applies it to 'this'
 function randomen () {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
@@ -69,11 +74,13 @@ function randomen () {
     this.style.backgroundColor = `rgb(${r},${g},${b})`;
 }
 
+// Firstly it removes all the listeners from the field, so you will not able to paint black and random color at the same time.
+// Secondly it turn on the 'random brush'
 const randomBtn = document.querySelector('#randomClr');
 randomBtn.addEventListener('click',stopListen);
 randomBtn.addEventListener('click',randomColor);
 
-
+// This applies 'blacken' func listener to all cells
 function blackColor() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
@@ -81,16 +88,18 @@ function blackColor() {
     })
 }
 
+// This creates a black color and applies it to 'this'
 function blacken () {
     this.style.backgroundColor = 'black';
 }
 
-
+// Firstly it removes all the listeners from the field, so you will not able to paint black and random color at the same time.
+// Secondly it turn on the 'black brush'
 const blackBtn = document.querySelector('#blackClr');
 blackBtn.addEventListener('click',stopListen);
 blackBtn.addEventListener('click',blackColor);
 
-
+// This applies 'brighten' func listener to all cells
 function grayColor() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
@@ -98,6 +107,7 @@ function grayColor() {
     })
 }
 
+// This set 'filter: brightness(XX%)' of the sell. It checks existing filter and changhes it if it's needed.
 function brighten() {
     if (this.style.filter == '') {
         this.style.filter = "brightness(90%)";
@@ -113,12 +123,13 @@ function brighten() {
     
 }
 
-
+// Firstly it removes all the listeners from the field, so you will not able to paint black and random color at the same time.
+// Secondly it turn on the 'dark brush'
 const grayBtn = document.querySelector('#grayClr');
 grayBtn.addEventListener('click',stopListen);
 grayBtn.addEventListener('click',grayColor);
 
-
+// Function which remove all listeners from the sells, so listeners cannot overlap each other
 function stopListen() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach( (cell) => {
